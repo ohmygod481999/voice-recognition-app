df3bf0ec35dc45d2e67619f00d61ad61d931b769
longvb <vuongbaolong48@gmail.com>
2020-05-17 00:12:00 +0700

    fix bug

--- google_regconition.py
+++ google_regconition.py
@@ -1,13 +1,14 @@
 import speech_recognition as sr
 r = sr.Recognizer()
 
-file = sr.AudioFile('blob.wav')
+file = sr.AudioFile('output.wav')
 with file as source:
     audio = r.record(source)
     
 print(audio)
 try:
-    text = r.recognize_google(audio, language = 'en-IN', show_all = True )
-    print("I thinks you said '" + r.recognize_google(audio) + "'")
+    text = r.recognize_google(audio, language="vi-VI", show_all = True )
+    print(text)
+    
 except LookupError:                               
     print("Could not understand audio")
